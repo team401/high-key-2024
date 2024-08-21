65ed939378722000f12d7f23a5d9ad1160357bec
Alexander <alexandercatalano@gmail.com>
2024-08-21 19:53:21 -0400

    vague idea of what shooter talon should look like

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+public class Constants {
+
+    public static final class SensorConstants {
+
+        //TODO: Find real values
+        public static final int indexerSensorPort = 0;
+    }
+    
+    public static final class ShooterConstants {
+
+        //TODO: Find real values
+        public static final int fromIntakeMotorID = 0;
+        public static final int pendulumMotorID = 0;
+        public static final int leftShooterMotorID = 0;
+        public static final int rightShooterMotorID = 0;
+
+        public static final double offsetFlywheelVelocities = 0;
+
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/scoring/ShooterIO.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.scoring;
+
+public interface ShooterIO {
+    
+    //@AutoLog
+    public static class ShooterIOInputs {
+
+        public double shooterLeftVelocityRPM = 0.0;
+        public double shooterLeftGoalVelocityRPM = 0.0;
+
+        public double shooterRightVelocityRPM = 0.0;
+        public double shooterRightGoalVelocityRPM = 0.0;
+
+        public double pendulumAngleRad = 0.0;
+        public double pendulumGoalAngleRad = 0.0;
+
+        public double fromIntakeMotorAppliedVolts = 0.0;
+    }
+
+    public default void updateInputs(ShooterIOInputs inputs) {};
+
+    public default void setShooterVelocity(double goalVelocity) {};
+
+    public default void setFromIntakeMotorVoltage(double kickerVoltage) {};
+
+    public default void setAimAngleRad(double angle) {};
+
+}
