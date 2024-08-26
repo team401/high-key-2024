061772401d43564c1cb7a725c67bc24c0642336f
Jack Lingle <captainjack0818@gmail.com>
2024-08-26 18:09:50 -0400

    fix format

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "high-key-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 5;
-    public static final String GIT_SHA = "86db87fe74699f023515ed649f83d53f329dcc3c";
-    public static final String GIT_DATE = "2024-08-24 10:02:41 EDT";
+    public static final int GIT_REVISION = 6;
+    public static final String GIT_SHA = "659c29ccbb5c0478c38182ec7a1970c4204886a8";
+    public static final String GIT_DATE = "2024-08-26 17:30:11 EDT";
     public static final String GIT_BRANCH = "basic-swerve";
-    public static final String BUILD_DATE = "2024-08-26 17:29:01 EDT";
-    public static final long BUILD_UNIX_TIME = 1724707741400L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2024-08-26 18:09:35 EDT";
+    public static final long BUILD_UNIX_TIME = 1724710175643L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
