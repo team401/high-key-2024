7d6dd446273357afd546d7ce216ec1cae4fd2540
minhnguyenbhs <minhnguyen@mcps.org>
2024-09-30 19:19:20 -0400

    shooter and singular no belt intake motor running

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "high-key-2024-2";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 17;
-    public static final String GIT_SHA = "adc9841b0a667d2e46e08d311345dac2ec3d8bc2";
-    public static final String GIT_DATE = "2024-09-25 18:24:22 EDT";
+    public static final int GIT_REVISION = 18;
+    public static final String GIT_SHA = "bf323bdc6ffafc7ecad2a41f714d10f0cb0ddf87";
+    public static final String GIT_DATE = "2024-09-25 19:55:59 EDT";
     public static final String GIT_BRANCH = "shooter-hardware";
-    public static final String BUILD_DATE = "2024-09-25 19:54:27 EDT";
-    public static final long BUILD_UNIX_TIME = 1727308467287L;
+    public static final String BUILD_DATE = "2024-09-30 19:11:23 EDT";
+    public static final long BUILD_UNIX_TIME = 1727737883104L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
