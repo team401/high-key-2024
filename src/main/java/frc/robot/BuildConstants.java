a489411cd49efadbe1cc6d351cbf7f465b17cf92
minhnguyenbhs <minhnguyen@mcps.org>
2024-10-02 18:10:46 -0400

    added banner sensor, untested

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "high-key-2024-2";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 18;
-    public static final String GIT_SHA = "bf323bdc6ffafc7ecad2a41f714d10f0cb0ddf87";
-    public static final String GIT_DATE = "2024-09-25 19:55:59 EDT";
+    public static final int GIT_REVISION = 19;
+    public static final String GIT_SHA = "c442c6bc9794075a9baecaad06937038df0adbea";
+    public static final String GIT_DATE = "2024-09-30 19:19:20 EDT";
     public static final String GIT_BRANCH = "shooter-hardware";
-    public static final String BUILD_DATE = "2024-09-30 19:11:23 EDT";
-    public static final long BUILD_UNIX_TIME = 1727737883104L;
+    public static final String BUILD_DATE = "2024-09-30 19:41:00 EDT";
+    public static final long BUILD_UNIX_TIME = 1727739660411L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
