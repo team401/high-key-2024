1681263e88a02b1dda7bcb0b6ab9d1e3c94c030f
minhnguyenbhs <minhnguyen@mcps.org>
2024-10-05 16:40:36 -0400

    Restore kinematics, flag out everything but drive.

--- src/main/java/frc/robot/constants/FeatureFlags.java
+++ src/main/java/frc/robot/constants/FeatureFlags.java
@@ -2,10 +2,10 @@ package frc.robot.constants;
 
 public final class FeatureFlags {
     public static final boolean runDrive = true;
-    public static final boolean runVision = true;
+    public static final boolean runVision = false;
     // NOTE: A featureflag "runLocalizer" was removed from here recently.
     // TODO: Figure out if we need this and add it back if necessary.
-    public static final boolean runLocalizer = true;
-    public static final boolean runIntake = true;
-    public static final boolean runScoring = true;
+    public static final boolean runLocalizer = false;
+    public static final boolean runIntake = false;
+    public static final boolean runScoring = false;
 }
